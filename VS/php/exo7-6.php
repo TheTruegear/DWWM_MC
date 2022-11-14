470d92bce0263eb68575e8c45d1a6b0a54ad85c1
TheTruegear <crequi.maxenceppgmail.com>
2022-11-14 16:17:46 +0100

    14 Novembre

--- VS/php/exo7-6.php
+++ VS/php/exo7-6.php
@@ -1,5 +1,21 @@
 <?php
-$tab1 = [1, 2, 3];
-$tab2 = [4, 5, 6];
-$tab1 + $tab2;
+$tab1 = [5, 2, 3, 4, 1, 9, 8];
+$tab2 = [7, 6, 10];
+$tab = [];
+$estVrai = true;
+$tab = array_merge($tab1, $tab2);
+while ($estVrai) {
+    $estVrai = false;
+    for ($i = 0; $i < count($tab) - 2; $i++) {
+        if ($tab[$i] > $tab[$i + 1]) {
+            $temp = $tab[$i];
+            $tab[$i] = $tab[$i + 1];
+            $tab[$i + 1] = $temp;
+            $estVrai = true;
+        }
+    }
+}
+foreach ($tab as $valeur){
+    echo $valeur . "\n";
+}
 ?>
---
+++ VS/php/exo9-1.php
@@ -0,0 +1,4 @@
+<?php
+$chomage = array("Autriche" =>4.9, "Allemagne" =>9.3, "Danemark" =>4.8 , "Espagne" =>9.4 , "France" =>9.7);
+echo ("Le nom du pays ayant le taux de chômage le plus faible")
+?>
