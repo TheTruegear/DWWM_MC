d6e80daa23a6f4b1851b864c6fa606ed8bffb49e
TheTruegear <crequi.maxenceppgmail.com>
2022-11-29 16:18:36 +0100

    29 Octobre

---
+++ VS/php/exo-chien-chat.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Wouf wouf miaou miaou</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <?php
+        $animal1 = array ("Nom"=>"Gazelle", "Age"=>"2", "Type"=>"Chat");
+        $animal2 = array ("Nom"=>"Idefix", "Age"=>"5", "Type"=>"Chien");
+        $tab=[$animal1, $animal2];
+        ?>
+    </body>
+</html>
