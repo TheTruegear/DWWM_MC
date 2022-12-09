890f589072a97cd0da317c5fc2ee19194e001f6e
TheTruegear <crequi.maxenceppgmail.com>
2022-12-09 11:55:05 +0100

    09 Decembre

--- HERITAGE/Exo/Employe.class.php
+++ HERITAGE/Exo/Employe.class.php
@@ -5,5 +5,34 @@ class Employe {
     protected $secu;
     protected $salaire;
     protected $job;
+
+    public function __construct($nom,$prenom,$secu,$salaire,$job){
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->secu = $secu;
+        $this->salaire = $salaire;
+        $this->job = $job;
+    }
+    public function getNom(){return $this->$nom;}
+    public function getPrenom(){return $this->$prenom;}
+    public function getSecu(){return $this->$secu;}
+    public function getSalaire(){return $this->$salaire;}
+    public function getJob(){return $this->$job;}
+
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+    public function setPrenom($prenom){
+        $this->prenom = $prenom;
+    }
+    public function setSecu($secu){
+        $this->secu = $secu;
+    }
+    public function setSalaire($salaire){
+        $this->salaire = $salaire;
+    }
+    public function setJob($job){
+        $this->job = $job;
+    }
 }
 ?>
